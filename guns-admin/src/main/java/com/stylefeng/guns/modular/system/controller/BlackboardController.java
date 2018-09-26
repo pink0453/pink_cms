e2cbd307d5ecfe10566f842a3f535c3027d4dcd9
admin <admin@DESKTOP-E1DTH6P>
2018-09-26 18:22:08 +0800

    commit

--- guns-admin/src/main/java/com/stylefeng/guns/modular/system/controller/BlackboardController.java
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/system/controller/BlackboardController.java
@@ -1,7 +1,14 @@
 package com.stylefeng.guns.modular.system.controller;
 
 import com.stylefeng.guns.core.base.controller.BaseController;
+import com.stylefeng.guns.core.common.exception.BizExceptionEnum;
+import com.stylefeng.guns.core.exception.GunsException;
+import com.stylefeng.guns.core.shiro.ShiroKit;
+import com.stylefeng.guns.core.util.ToolUtil;
+import com.stylefeng.guns.modular.system.model.User;
 import com.stylefeng.guns.modular.system.service.INoticeService;
+import com.stylefeng.guns.modular.system.service.IUserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -22,6 +29,9 @@ public class BlackboardController extends BaseController {
 
     @Autowired
     private INoticeService noticeService;
+    
+    @Autowired
+    private IUserService userService;
 
     /**
      * 跳转到黑板
@@ -30,6 +40,15 @@ public class BlackboardController extends BaseController {
     public String blackboard(Model model) {
         List<Map<String, Object>> notices = noticeService.list(null);
         model.addAttribute("noticeList", notices);
+        
+        Integer userId = ShiroKit.getUser().getId();
+        if (ToolUtil.isEmpty(userId)) {
+            throw new GunsException(BizExceptionEnum.REQUEST_NULL);
+        }
+        User user = this.userService.selectById(userId);
+        
+        model.addAttribute(user);
+        
         return "/blackboard.html";
     }
 }
